3792ba34fafe5c7fa4e651ed7ac6ae36a33618ab
chenyunhui <chenyunhui@corp.netease.com>
2020-07-01 17:19:44 +0800

    new

--- curve-snapshotcloneserver-nginx/app/src/init.lua
+++
@@ -1,8 +0,0 @@
--- Copyright (c) Netease
-
-require "resty.core"
-local checkups = require "resty.checkups.api"
-config = require "config"
-
-config.global.version = "v0.0.1"
-checkups.init(config)
--- curve-snapshotcloneserver-nginx/app/src/snapshot.lua
+++
@@ -1,3 +0,0 @@
-local httproxy = require "modules.httproxy"
-
-return httproxy.exec(ngx.var.request_method, ngx.var.request_uri, "snapshot");
